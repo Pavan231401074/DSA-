4d03eff0293b188dba128c29c48487f0eb5afcfc
Pavan231401074 <231401074@rajalakshmi.edu.in>
2024-06-17 01:35:56 +0530

    Update 5.b.c

--- 5.b.c
+++ 5.b.c
@@ -2,21 +2,13 @@
 
 #include <stdlib.h>
 
-#include <ctype.h>
+typedef struct Stack {
 
-#include <string.h>
-
-
-
-#define MAX 100
-
-
-
-typedef struct {
-
-    int items[MAX];
+    int *items;
 
     int top;
+
+    int max_size;
 
 } Stack;
 
@@ -24,13 +16,15 @@ typedef struct {
 
 // Function to initialize the stack
 
-void initialize(Stack *s) {
+void initialize(Stack *s, int max_size) {
+
+    s->items = (int *)malloc(max_size * sizeof(int));
 
     s->top = -1;
 
+    s->max_size = max_size;
+
 }
-
-
 
 // Function to check if the stack is empty
 
@@ -40,23 +34,11 @@ int isEmpty(Stack *s) {
 
 }
 
-
-
-// Function to check if the stack is full
-
-int isFull(Stack *s) {
-
-    return s->top == MAX - 1;
-
-}
-
-
-
 // Function to push an element onto the stack
 
 void push(Stack *s, int newItem) {
 
-    if (isFull(s)) {
+    if (s->top == s->max_size - 1) {
 
         printf("Stack is full. Cannot push %d\n", newItem);
 
@@ -88,163 +70,41 @@ int pop(Stack *s) {
 
 
 
-// Function to get the precedence of operators
+// Function to display the move
 
-int precedence(char operator) {
-        case '+':
+void displayMove(char source, char destination, int disk) {
 
-        case '-':
-
-            return 1;
-
-        case '*':
-
-        case '/':
-
-            return 2;
-
-        case '^':
-
-            return 3;
-
-        default:
-
-            return 0;
-
-    }
+    printf("Move disk %d from %c to %c\n", disk, source, destination);
 
 }
 
 
 
-// Function to apply an operator to two operands
+// Recursive function to solve Tower of Hanoi problem
 
-int applyOperator(int operand1, char operator, int operand2) {
+void towerOfHanoi(int numDisks, Stack *source, Stack *auxiliary, Stack *destination, char s, char a, char d) {
 
-    switch (operator) {
+    if (numDisks == 1) {
 
-        case '+':
+        int disk = pop(source);
 
-            return operand1 + operand2;
+        push(destination, disk);
 
-        case '-':
+        displayMove(s, d, disk);
 
-            return operand1 - operand2;
-
-        case '*':
-
-            return operand1 * operand2;
-
-        case '/':
-
-            return operand1 / operand2;
-
-        case '^':
-
-            return (int)pow(operand1, operand2);
-
-        default:
-
-            return 0;
+        return;
 
     }
-
-}
-
-/ / Function to evaluate an infix expression
-
-int evaluateInfix(char* expression) {
-
-    Stack operands, operators;
-
-    initialize(&operands);
-
-    initialize(&operators);
-
     
+    towerOfHanoi(numDisks - 1, source, destination, auxiliary, s, d, a);
 
-    for (int i = 0; i < strlen(expression); i++) {
+    int disk = pop(source);
 
-        if (expression[i] == ' ')
+    push(destination, disk);
 
-            continue;
+    displayMove(s, d, disk);
 
-        
-
-        if (isdigit(expression[i])) {
-
-            int value = 0;
-
-            while (i < strlen(expression) && isdigit(expression[i])) {
-
-                value = (value * 10) + (expression[i] - '0');
-
-                i++;
-
-            }
-
-            i--;
-
-            push(&operands, value);
-
-    switch (operator) {
- } else if (expression[i] == '(') {
-
-            push(&operators, expression[i]);
-
-        } else if (expression[i] == ')') {
-
-            while (!isEmpty(&operators) && operators.items[operators.top] != '(') {
-
-                int operand2 = pop(&operands);
-
-                int operand1 = pop(&operands);
-
-                char operator = pop(&operators);
-
-                push(&operands, applyOperator(operand1, operator, operand2));
-
-            }
-
-            pop(&operators); // Remove the '('
-
-        } else {
-
-            while (!isEmpty(&operators) && precedence(operators.items[operators.top]) >= precedence(expression[i])) {
-
-                int operand2 = pop(&operands);
-
-                int operand1 = pop(&operands);
-
-                char operator = pop(&operators);
-
-                push(&operands, applyOperator(operand1, operator, operand2));
-
-            }
-
-            push(&operators, expression[i]);
-
-        }
-
-    }
-
-
-
-    while (!isEmpty(&operators)) {
-
-        int operand2 = pop(&operands);
-
-        int operand1 = pop(&operands);
-
-        char operator = pop(&operators);
-
-        push(&operands, applyOperator(operand1, operator, operand2));
-
-    }
-
-
-
-    return pop(&operands);
+    towerOfHanoi(numDisks - 1, auxiliary, source, destination, a, s, d);
 
 }
 
@@ -252,19 +112,45 @@ int evaluateInfix(char* expression) {
 
 int main() {
 
-    char expression[MAX];
+    int numDisks;
 
-    printf("Enter the infix expression: ");
+    printf("Enter the number of disks: ");
 
-    fgets(expression, MAX, stdin);
-
-    expression[strcspn(expression, "\n")] = '\0';
+    scanf("%d", &numDisks);
 
 
 
-    int result = evaluateInfix(expression);
+    Stack source, auxiliary, destination;
 
-    printf("Result of infix expression evaluation: %d\n", result);
+    initialize(&source, numDisks);
+
+    initialize(&auxiliary, numDisks);
+
+    initialize(&destination, numDisks);
+
+
+
+    for (int i = numDisks; i >= 1; i--) {
+
+        push(&source, i);
+
+    }
+
+
+
+    printf("Moves to solve the Tower of Hanoi with %d disks:\n", numDisks);
+
+    towerOfHanoi(numDisks, &source, &auxiliary, &destination, 'A', 'B', 'C');
+
+
+
+    // Free allocated memory
+
+    free(source.items);
+
+    free(auxiliary.items);
+
+    free(destination.items);
 
 
 
